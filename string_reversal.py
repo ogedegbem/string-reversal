b308336b9ece2d702a31b8a9d4ce7c8545a94fdf
Maurice Ogedegbe <83469006+ogedegbem@users.noreply.github.com>
2023-01-06 14:25:58 +0100

    Add files via upload

---
+++ string_reversal.py
@@ -0,0 +1,6 @@
+def reverse_string(str1):
+    """Write your code here to reverse the string"""
+    return str1 [:: -1]
+    raise NotImplementedError()
+
+# test cases are 'Colchester','University'
